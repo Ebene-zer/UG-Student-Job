654df87b88f9710ec1f99bf78ca9e225de326f8e
Ebenezer Fuachie <fuachiee717@gmail.com>
2026-03-04 12:03:55 +0000

    Initial Django setup with PostgreSQL

---
+++ .env_example.py
@@ -0,0 +1,8 @@
+# Keep envrionment variables here. DO NOT commit this file to version control.
+# Example:
+# SECRET_KEY='your-django-secret-key'
+# DB_NAME='the db name'
+# DB_USER='the db user'
+# DB_PASSWORD='the db password'
+# DB_HOST='the db host'
+# DB_PORT='the db port'
