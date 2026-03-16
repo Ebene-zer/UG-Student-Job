e6f9bdfbb978d8ebc2ba14f46e665011766ed70e
Ebenezer Fuachie <fuachiee717@gmail.com>
2026-03-16 20:51:37 +0000

    feat(applications):  add application models and relationships

--- applications/admin.py
+++ applications/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Application, CV, StudentSkill, ApplicationStatusHistory, InterviewSchedule
 
 # Register your models here.
+admin.site.register(Application)
+admin.site.register(CV)
+admin.site.register(StudentSkill)
+admin.site.register(ApplicationStatusHistory)
+admin.site.register(InterviewSchedule)
