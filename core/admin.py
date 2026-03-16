58a538b97ae78af338dc4b3270ce0f7d72d78204
Ebenezer Fuachie <fuachiee717@gmail.com>
2026-03-16 21:15:57 +0000

    feat(core):  add core models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Notification, SavedJob, Report, AuditLog
 
 # Register your models here.
+admin.site.register(Notification)
+admin.site.register(SavedJob)
+admin.site.register(Report)
+admin.site.register(AuditLog)
