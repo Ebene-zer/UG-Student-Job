714b8a7bf52c2f71915652bfc344a8973bda4e01
Ebenezer Fuachie <fuachiee717@gmail.com>
2026-03-20 22:17:37 +0000

    add default placceholder for uncompleted apps

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import JobListView
+from core.views import placeholder
 
 urlpatterns = [
-    path('jobs/', JobListView.as_view()),
+    path('', lambda request: placeholder(request, "Jobs"), name='jobs-placeholder'),
+    # path('jobs/', JobListView.as_view()),
 ]
